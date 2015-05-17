3a5675c6ed340742889042a72b021e47f80a69f4
Luke Karrys <lukekarrys@gmail.com>
2015-05-16 22:26:37 -0700

    src

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import React from 'react-native';
+import StyleSheet from 'StyleSheet';
+
+const {MapView, View} = React;
+
+const styles = StyleSheet.create({
+  map: {
+    height: 302,
+    marginTop: 20
+  }
+});
+
+const AppMapView = React.createClass({
+  getInitialState () {
+    return {
+      annotations: null,
+      isFirstLoad: true
+    };
+  },
+
+  render () {
+    return (
+      <View>
+        <MapView
+          style={styles.map}
+          onRegionChangeComplete={this._onRegionChangeComplete}
+          annotations={this.state.annotations}
+          showsUserLocation={true}
+        />
+      </View>
+    );
+  },
+
+  _getAnnotations (region) {
+    return [{
+      longitude: region.longitude,
+      latitude: region.latitude,
+      title: 'You Are Here'
+    }, {
+      longitude: region.longitude + 2,
+      latitude: region.latitude + 2,
+      title: 'You Are Here 2'
+    }];
+  },
+
+  _onRegionChangeComplete (region) {
+    if (this.state.isFirstLoad) {
+      this.setState({
+        annotations: this._getAnnotations(region),
+        isFirstLoad: false
+      });
+    }
+  }
+});
+
+export default AppMapView;
---
+++ src/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import React from 'react-native';
+import App from './components/App';
+
+const {AppRegistry} = React;
+
+AppRegistry.registerComponent('geohashing', () => App);
