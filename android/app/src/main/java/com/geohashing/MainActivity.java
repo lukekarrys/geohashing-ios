3d564120cfcddb06f81888efbbf0428e2b31abaf
Luke Karrys <lukekarrys@gmail.com>
2016-08-10 09:24:55 -0700

    upgrade

--- android/app/src/main/java/com/geohashing/MainActivity.java
+++ android/app/src/main/java/com/geohashing/MainActivity.java
@@ -1,13 +1,6 @@
 package com.geohashing;
 
 import com.facebook.react.ReactActivity;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.devfd.RNGeocoder.RNGeocoderPackage;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
@@ -18,27 +11,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "geohashing";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-   /**
-   * A list of packages used by the app. If the app uses additional views
-   * or modules besides the default ones, add more packages here.
-   */
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new VectorIconsPackage(),
-        new RNGeocoderPackage()
-      );
     }
 }
